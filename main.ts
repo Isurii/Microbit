40ba334f46001007534ff24bd999c3b229e9a00a
Isurii <isuri.yahampath2@gmail.com>
2020-07-07 10:59:23 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.showNumber(1)
+basic.pause(300)
+basic.showString("Isuri")
+basic.pause(300)
+basic.forever(function () {
+    basic.showNumber(2)
+    basic.showIcon(IconNames.Asleep)
+    basic.showArrow(ArrowNames.East)
+})
